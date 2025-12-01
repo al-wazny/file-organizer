59df1ca6528beefb53e06e30f3ef1814e37063c5
ali.al-wazny <ali.al-wazny@flagbit.de>
2025-12-01 01:46:47 +0100

    feat: Parse optional path argument in cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(short, long, default_value = ".")]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?}", args.path);
 }
