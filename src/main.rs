62b25570b2c85a185a8e9ffcf8de3bae2bdf97d7
ali.al-wazny <ali.al-wazny@flagbit.de>
2025-12-01 21:55:17 +0100

    feat: Add more optional arguments

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ use clap::Parser;
 struct Cli {
     #[clap(short, long, default_value = ".")]
     path: String,
+
+    #[clap(short, long, num_args(0..))]
+    extensions: Vec<String>,
+    
+    #[clap(short, long)]
+    dry_run: bool,
+    
+    #[clap(short, long)]
+    backup: bool,
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("{:?}", args.path);
+    println!("{:#?}", args);
 }
