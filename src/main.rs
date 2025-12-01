7dbf26fd01a4e27b35bd7ea9eccec71036bca152
ali.al-wazny <ali.al-wazny@flagbit.de>
2025-12-01 23:23:29 +0100

    feat: List directory content of given path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::fs::{self, ReadDir};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -15,7 +16,22 @@ struct Cli {
     backup: bool,
 }
 
+fn get_dir_entries(entries: ReadDir) {
+    for entry in entries {
+        if let Ok(_entry) = entry {
+            println!("{:?}", _entry.path());
+        }
+    }
+}
+
 fn main() {
     let args = Cli::parse();
-    println!("{:#?}", args);
+    let directory = fs::read_dir(args.path);
+
+    match directory {
+        Ok(_dir) => {
+            get_dir_entries(_dir);
+        }
+        Err(e) => { println!("Error opening directory: {}", e); }
+    }
 }
