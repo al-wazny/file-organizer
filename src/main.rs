078b72c8ca483d60ba5569bb55252df6e0e8b5f1
ali.al-wazny <ali.al-wazny@flagbit.de>
2025-12-16 00:52:55 +0100

    fix: Don't panic when there is a file with no extension and clean up some code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#![allow(dead_code, unused_variables)]
+
 use clap::Parser;
 use regex::Regex;
-use std::ffi::{OsString};
-use std::fs::{self};
 use serde_json::Value;
 use std::env;
+use std::ffi::OsString;
+use std::fs::{self};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -13,10 +15,10 @@ struct Cli {
 
     #[clap(short, long, num_args(0..))]
     extensions: Vec<String>,
-    
+
     #[clap(short, long)]
     dry_run: bool,
-    
+
     #[clap(short, long)]
     backup: bool,
 }
@@ -24,7 +26,7 @@ struct Cli {
 #[derive(Debug)]
 struct File {
     extension: OsString,
-    file_name: OsString,
+    name: OsString,
     current_path: PathBuf,
     new_path: Option<PathBuf>,
 }
@@ -32,29 +34,28 @@ struct File {
 fn get_dir_entries(directory_path: &PathBuf) -> Option<Vec<File>> {
     fs::read_dir(directory_path).ok().map(|dir| {
         dir.filter_map(Result::ok)
-            .filter(|entry| entry.file_type()
-            .map(|ft| ft.is_file()).unwrap_or(false))
+            .filter(|entry| entry.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+            .filter(|entry| entry.path().extension().is_some())
             .map(|entry| {
                 let path = entry.path();
                 File {
                     extension: path.extension().unwrap().to_owned(),
-                    file_name: path.file_stem().unwrap().to_owned(),
+                    name: path.file_stem().unwrap().to_owned(),
                     current_path: path.as_path().to_owned(),
                     new_path: None,
                 }
-            }).collect()
+            })
+            .collect()
     })
 }
-
 
 fn get_config() -> Value {
     const PROJECT_DIR: &str = env!("CARGO_MANIFEST_DIR");
     let mut config_path = format!("{}/src/config.json", PROJECT_DIR);
     config_path = fs::read_to_string(&config_path).unwrap();
-    
+
     serde_json::from_str(&config_path).unwrap()
 }
-
 
 // search json recursively and return configured path if given target is set inside config.json
 fn get_configured_path(json: &Value, file: &File, path: &String) -> Option<String> {
@@ -69,7 +70,11 @@ fn get_configured_path(json: &Value, file: &File, path: &String) -> Option<Strin
         }
         Value::Object(obj) => {
             for (key, value) in obj.iter() {
-                let new_path = if path.is_empty() { key.to_string() } else { format!("{}/{}", path, key) };
+                let new_path = if path.is_empty() {
+                    key.to_string()
+                } else {
+                    format!("{}/{}", path, key)
+                };
                 if let Some(p) = get_configured_path(value, file, &new_path) {
                     return Some(p);
                 }
@@ -79,14 +84,19 @@ fn get_configured_path(json: &Value, file: &File, path: &String) -> Option<Strin
         _ => {
             // (fix) regex needs to be specified in the config file before the file extensions
             // (chore) clean this messy shit up later on to make it somewhat human readable
-            if json.as_str() == file.extension.to_str() || 
-                Regex::new(json.as_str()?).unwrap().is_match(file.file_name.to_str()?) {
-                Some(path.to_owned()) 
-            } else { None } 
+            let json_value = json.as_str();
+            if json_value == file.extension.to_str()
+                || Regex::new(json_value?)
+                    .unwrap()
+                    .is_match(file.name.to_str()?)
+            {
+                Some(path.to_owned())
+            } else {
+                None
+            }
         }
     }
 }
-
 
 fn get_absolute_path(rel_path: &String) -> PathBuf {
     let relative = PathBuf::from(rel_path);
@@ -96,11 +106,10 @@ fn get_absolute_path(rel_path: &String) -> PathBuf {
     absolute_path
 }
 
-
 fn main() {
     let args = Cli::parse();
     let directory_path = PathBuf::from(args.path);
-    
+
     if let Some(mut entries) = get_dir_entries(&directory_path) {
         entries.iter_mut().for_each(|entry| {
             let config = get_config();
@@ -111,5 +120,5 @@ fn main() {
                 println!("{:#?}", entry);
             }
         });
-    } 
+    }
 }
